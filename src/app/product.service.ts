872988ac4192c325fc8a77e145c7ffbff39245b7
tokisakirumi <karmalangpo@gmail.com>
2023-04-11 10:13:19 +0800

    modified errors

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+
 
 
 @Injectable()
@@ -10,10 +10,8 @@ export class ProductService {
 
   constructor(private _http: Http) {  }
 
-  getAlbum(id: number): Observable<any> {
-    return this._http.get(this._albumUrl).pipe(
-      map((response: Response) => response.json())
-    );
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
   
 
